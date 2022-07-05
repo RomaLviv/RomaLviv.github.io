23b5d76376c7d99d6e96e9b326d4bf625ea49538
Roman <chajkovskyj.roman@gmail.com>
2022-07-05 17:34:09 +0300

    Slider
    swiper

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+window.onload = function() {
 const items = document.querySelectorAll(".accordion button");
 
 items.forEach((item) => item.addEventListener("click", toggleAccordion));
@@ -7,9 +9,38 @@ function toggleAccordion() {
 
   for (let item of items) {
     item.setAttribute("aria-expanded", false);
-  }
+  };
 
   if (itemToggle === "false") {
     this.setAttribute("aria-expanded", true);
-  }
-}
+  };
+};
+const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+// Responsive breakpoints
+breakpoints: {
+// when window width is >= 480px
+480: {
+  slidesPerView: 1,
+},
+// when window width is >= 769px
+769: {
+  slidesPerView: 1,
+},
+// when window width is >= 960px
+960: {
+  slidesPerView: 2,
+},
+},
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+};
+
+
